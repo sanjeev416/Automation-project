abc338868905a63e2199b10e3d826b5ba05a6c61
sanjeev416 <sanjeevar416@gmail.com>
2019-10-18 14:42:15 +0530

    updated code

---
+++ automationpractise/src/main/java/com/cucumber/pom/Homepage.java
@@ -0,0 +1,32 @@
+package com.cucumber.pom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+import com.cucumber.baseclass.Basepage;
+
+
+
+public class Homepage extends Basepage {
+    
+	@FindBys(@FindBy (xpath = "//div[@id='block_top_menu']/ul/li[1]/a"))
+	private WebElement Womentab;
+	
+	public Homepage() 
+	{
+	PageFactory.initElements(driver, this);
+
+}
+	
+	 public WebElement getwomen()
+		{
+			return Womentab;
+		}
+
+
+
+	
+
+}
---
+++ automationpractise/src/test/java/com/cucumber/feature/automation.feature
@@ -0,0 +1,5 @@
+Feature: smoke test automation script
+Scenario: user should login womens page in automation practise website
+Given user should login the womens page 
+When user should the click the womens tab
+Then user should verify the womens tab
